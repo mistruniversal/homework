5c35ecf1cc2fc530786412195a43e77b3c2630ec
mistruniversal <152072458+mistruniversal@users.noreply.github.com>
2024-08-29 22:08:33 +0400

    Add files via upload

---
+++ homework35.py
@@ -0,0 +1,305 @@
+my_items = [
+    {'name': 'bread', 'price': 0.5, 'quantity': 20},
+    {'name': 'milk', 'price': 1.0, 'quantity': 10},
+    {'name': 'wine', 'price': 10.0, 'quantity': 5},
+]
+
+items = list()
+
+
+def create_items(app_items):
+    global items
+    items = app_items
+
+
+def create_item(name, price, quantity):
+    global items
+    items.append({'name': name, 'price': price, 'quantity': quantity})
+
+def read_item(name):
+    global items
+    myitems = list(filter(lambda x: x['name'] == name, items))
+    return myitems[0]
+
+
+def read_items():
+    global items
+    return [item for item in items]
+
+class ItemAlreadyStored(Exception):
+    pass
+
+
+class ItemNotStored(Exception):
+    pass
+
+import mvc_exceptions as mvc_exc
+
+items = list()
+
+
+def create_item(name, price, quantity):
+    global items
+    results = list(filter(lambda x: x['name'] == name, items))
+    if results:
+        raise mvc_exc.ItemAlreadyStored('"{}" already stored!'.format(name))
+    else:
+        items.append({'name': name, 'price': price, 'quantity': quantity})
+
+
+def create_items(app_items):
+    global items
+    items = app_items
+
+
+def read_item(name):
+    global items
+    myitems = list(filter(lambda x: x['name'] == name, items))
+    if myitems:
+        return myitems[0]
+    else:
+        raise mvc_exc.ItemNotStored(
+            'Can\'t read "{}" because it\'s not stored'.format(name))
+
+
+def read_items():
+    global items
+    return [item for item in items]
+
+def update_item(name, price, quantity):
+    global items
+    # Python 3.x removed tuple parameters unpacking (PEP 3113), so we have to do it manually (i_x is a tuple, idxs_items is a list of tuples)
+    idxs_items = list(
+        filter(lambda i_x: i_x[1]['name'] == name, enumerate(items)))
+    if idxs_items:
+        i, item_to_update = idxs_items[0][0], idxs_items[0][1]
+        items[i] = {'name': name, 'price': price, 'quantity': quantity}
+    else:
+        raise mvc_exc.ItemNotStored(
+            'Can\'t update "{}" because it\'s not stored'.format(name))
+
+
+def delete_item(name):
+    global items
+    # Python 3.x removed tuple parameters unpacking (PEP 3113), so we have to do it manually (i_x is a tuple, idxs_items is a list of tuples)
+    idxs_items = list(
+        filter(lambda i_x: i_x[1]['name'] == name, enumerate(items)))
+    if idxs_items:
+        i, item_to_delete = idxs_items[0][0], idxs_items[0][1]
+        del items[i]
+    else:
+        raise mvc_exc.ItemNotStored(
+            'Can\'t delete "{}" because it\'s not stored'.format(name))
+
+def main():
+
+    my_items = [
+        {'name': 'bread', 'price': 0.5, 'quantity': 20},
+        {'name': 'milk', 'price': 1.0, 'quantity': 10},
+        {'name': 'wine', 'price': 10.0, 'quantity': 5},
+    ]
+
+    # CREATE
+    create_items(my_items)
+    create_item('beer', price=3.0, quantity=15)
+    # if we try to re-create an object we get an ItemAlreadyStored exception
+    # create_item('beer', price=2.0, quantity=10)
+
+    # READ
+    print('READ items')
+    print(read_items())
+    # if we try to read an object not stored we get an ItemNotStored exception
+    # print('READ chocolate')
+    # print(read_item('chocolate'))
+    print('READ bread')
+    print(read_item('bread'))
+
+    # UPDATE
+    print('UPDATE bread')
+    update_item('bread', price=2.0, quantity=30)
+    print(read_item('bread'))
+    # if we try to update an object not stored we get an ItemNotStored exception
+    # print('UPDATE chocolate')
+    # update_item('chocolate', price=10.0, quantity=20)
+
+    # DELETE
+    print('DELETE beer')
+    delete_item('beer')
+    # if we try to delete an object not stored we get an ItemNotStored exception
+    # print('DELETE chocolate')
+    # delete_item('chocolate')
+
+    print('READ items')
+    print(read_items())
+
+if __name__ == '__main__':
+    main()
+
+import basic_backend
+import mvc_exceptions as mvc_exc
+
+
+class ModelBasic(object):
+
+    def __init__(self, application_items):
+        self._item_type = 'product'
+        self.create_items(application_items)
+
+    @property
+    def item_type(self):
+        return self._item_type
+
+    @item_type.setter
+    def item_type(self, new_item_type):
+        self._item_type = new_item_type
+
+    def create_item(self, name, price, quantity):
+        basic_backend.create_item(name, price, quantity)
+
+    def create_items(self, items):
+        basic_backend.create_items(items)
+
+    def read_item(self, name):
+        return basic_backend.read_item(name)
+
+    def read_items(self):
+        return basic_backend.read_items()
+
+    def update_item(self, name, price, quantity):
+        basic_backend.update_item(name, price, quantity)
+
+    def delete_item(self, name):
+        basic_backend.delete_item(name)
+
+class View(object):
+
+    @staticmethod
+    def show_bullet_point_list(item_type, items):
+        print('--- {} LIST ---'.format(item_type.upper()))
+        for item in items:
+            print('* {}'.format(item))
+
+    @staticmethod
+    def show_number_point_list(item_type, items):
+        print('--- {} LIST ---'.format(item_type.upper()))
+        for i, item in enumerate(items):
+            print('{}. {}'.format(i+1, item))
+
+    @staticmethod
+    def show_item(item_type, item, item_info):
+        print('//////////////////////////////////////////////////////////////')
+        print('Good news, we have some {}!'.format(item.upper()))
+        print('{} INFO: {}'.format(item_type.upper(), item_info))
+        print('//////////////////////////////////////////////////////////////')
+
+    @staticmethod
+    def display_missing_item_error(item, err):
+        print('**************************************************************')
+        print('We are sorry, we have no {}!'.format(item.upper()))
+        print('{}'.format(err.args[0]))
+        print('**************************************************************')
+
+    @staticmethod
+    def display_item_already_stored_error(item, item_type, err):
+        print('**************************************************************')
+        print('Hey! We already have {} in our {} list!'
+              .format(item.upper(), item_type))
+        print('{}'.format(err.args[0]))
+        print('**************************************************************')
+
+    @staticmethod
+    def display_item_not_yet_stored_error(item, item_type, err):
+        print('**************************************************************')
+        print('We don\'t have any {} in our {} list. Please insert it first!'
+              .format(item.upper(), item_type))
+        print('{}'.format(err.args[0]))
+        print('**************************************************************')
+
+    @staticmethod
+    def display_item_stored(item, item_type):
+        print('++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++')
+        print('Hooray! We have just added some {} to our {} list!'
+              .format(item.upper(), item_type))
+        print('++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++')
+
+    @staticmethod
+    def display_change_item_type(older, newer):
+        print('---   ---   ---   ---   ---   ---   ---   ---   ---   ---   --')
+        print('Change item type from "{}" to "{}"'.format(older, newer))
+        print('---   ---   ---   ---   ---   ---   ---   ---   ---   ---   --')
+
+    @staticmethod
+    def display_item_updated(item, o_price, o_quantity, n_price, n_quantity):
+        print('---   ---   ---   ---   ---   ---   ---   ---   ---   ---   --')
+        print('Change {} price: {} --> {}'
+              .format(item, o_price, n_price))
+        print('Change {} quantity: {} --> {}'
+              .format(item, o_quantity, n_quantity))
+        print('---   ---   ---   ---   ---   ---   ---   ---   ---   ---   --')
+
+    @staticmethod
+    def display_item_deletion(name):
+        print('--------------------------------------------------------------')
+        print('We have just removed {} from our list'.format(name))
+        print('--------------------------------------------------------------')
+
+class Controller(object):
+
+    def __init__(self, model, view):
+        self.model = model
+        self.view = view
+
+    def show_items(self, bullet_points=False):
+        items = self.model.read_items()
+        item_type = self.model.item_type
+        if bullet_points:
+            self.view.show_bullet_point_list(item_type, items)
+        else:
+            self.view.show_number_point_list(item_type, items)
+
+    def show_item(self, item_name):
+        try:
+            item = self.model.read_item(item_name)
+            item_type = self.model.item_type
+            self.view.show_item(item_type, item_name, item)
+        except mvc_exc.ItemNotStored as e:
+            self.view.display_missing_item_error(item_name, e)
+
+    def insert_item(self, name, price, quantity):
+        assert price > 0, 'price must be greater than 0'
+        assert quantity >= 0, 'quantity must be greater than or equal to 0'
+        item_type = self.model.item_type
+        try:
+            self.model.create_item(name, price, quantity)
+            self.view.display_item_stored(name, item_type)
+        except mvc_exc.ItemAlreadyStored as e:
+            self.view.display_item_already_stored_error(name, item_type, e)
+
+    def update_item(self, name, price, quantity):
+        assert price > 0, 'price must be greater than 0'
+        assert quantity >= 0, 'quantity must be greater than or equal to 0'
+        item_type = self.model.item_type
+
+        try:
+            older = self.model.read_item(name)
+            self.model.update_item(name, price, quantity)
+            self.view.display_item_updated(
+                name, older['price'], older['quantity'], price, quantity)
+        except mvc_exc.ItemNotStored as e:
+            self.view.display_item_not_yet_stored_error(name, item_type, e)
+            # if the item is not yet stored and we performed an update, we have
+            # 2 options: do nothing or call insert_item to add it.
+            # self.insert_item(name, price, quantity)
+
+    def update_item_type(self, new_item_type):
+        old_item_type = self.model.item_type
+        self.model.item_type = new_item_type
+        self.view.display_change_item_type(old_item_type, new_item_type)
+
+    def delete_item(self, name):
+        item_type = self.model.item_type
+        try:
+            self.model.delete_item(name)
+            self.view.display_item_deletion(name)
+        except mvc_exc.ItemNotStored as e:
+            self.view.display_item_not_yet_stored_error(name, item_type, e)
